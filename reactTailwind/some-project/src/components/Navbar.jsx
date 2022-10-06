463dc43084f04d1c902fb0076300e491d340a1b6
Ramazan Zholdas <ghostcomrad@gmail.com>
2022-10-06 21:22:32 +0600

    doing rubezhka

--- reactTailwind/some-project/src/components/Navbar.jsx
+++ reactTailwind/some-project/src/components/Navbar.jsx
@@ -1,12 +1,18 @@
 //create navbar
-import React from 'react';
+import React, {useState} from 'react';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 
 const Navbar = () => {
-    return (
-        <div className='text-white'>
-            <h1>React</h1>
-        </div>
-    );
-}
-    
+  return (
+    <div className="flex justify-between items-center h-24 text-white max-w-5xl mx-auto px-4">
+      <h1 className="w-full text-3xl font-bold text-green-400">REACT.</h1>
+      <ul className="flex">
+        <li className="p-4">Home</li>
+        <li className="p-4">About</li>
+        <li className="p-4">Contact</li>
+      </ul>
+    </div>
+  );
+};
+
 export default Navbar;
