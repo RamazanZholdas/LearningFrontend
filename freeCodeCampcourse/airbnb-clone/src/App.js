8cb616d75786f3d3c20b7cb18fd519ac418ffbf8
Ramazan Zholdas <ghostcomrad@gmail.com>
2022-10-09 17:44:28 +0600

    airbnb clone idk

---
+++ freeCodeCampcourse/airbnb-clone/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+	  <h1 className='text-3xl text-blue-700 m-24'>Siski Zhibek prekrasni</h1>
+    </div>
+  );
+}
+
+export default App
